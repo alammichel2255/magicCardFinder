82113bc581cb169f3b1b5a2405a6c753b31aaefd
Eric Sheridan <neobelkins@gmail.com>
2022-08-11 13:28:37 -0700

    got the scryfallFetch method working

--- scripts/cardDisplay.js
+++ scripts/cardDisplay.js
@@ -1,40 +1,64 @@
-
+import {ScryfallFetch} from "./scryfallFetch.js";
 
 window.addEventListener('DOMContentLoaded', (event) => {
   console.log('DOM fully loaded and parsed');
 });
 
 
-// https://api.scryfall.com/cards/6da045f8-6278-4c84-9d39-025adf0789c1
+const buildPage = async() => {
+  let card = await ScryfallFetch.getRandom();
+  let cardOracleTxt = card.oracle_text;
+  let cardColors = card.colors;
+  let cardImgPng = card.image_uris.png;
+  let cardImgArt = card.image_uris.art_crop;
+  let cardType = card.type_line;
+  let cardRarity = card.rarity;
+  let cardFlavorText = card.flavor_text;
+  let cardArtist = card.artist_ids[0];
+  let cardName = card.name;
+  // console.log(card)
 
-// document.getElementById('bg-image').innerHTML = ('<div id="bg-image"></div>');
-
-
-
-
-
-//fetch card object
-fetch('https://api.scryfall.com/cards/44865244-2b9f-4734-a4da-49613b23ee4d')
-  .then((response) => response.json())
-  .then((data) => {
-    //grab needed data
-    let cardOracleTxt = data.oracle_text;
-    let cardColors = data.colors;
-    let cardImgPng = data.image_uris.png;
-    let cardImgArt = data.image_uris.art_crop;
-    let cardType = data.type_line;
-    let cardRarity = data.rarity;
-    let cardFlavorText = data.flavor_text;
-    let cardArtist = data.artist_ids[0];
-    let cardName = data.name;
-
-
-    document.getElementById("bg-image").style.backgroundImage = `url(${cardImgArt})`;
-    
-    let pageHtml = `<div class="main-content"><div class="card-image"><img src=${cardImgPng} alt="card image" class="responsive" id="cardImg"></div><div class="text-box" id="testBox">${cardName}: ${cardColors}<br><hr class="solid">Type: ${cardType}, Rarity: ${cardRarity}<br><hr class="solid">${cardOracleTxt}<br><hr class="solid"><i>${cardFlavorText}</i><br><hr class="dotted">Artist: ${cardArtist}</div></div>`;
+  document.getElementById("bg-image").style.backgroundImage = `url(${cardImgArt})`;
+  
+  let pageHtml = `
+<div class="main-content">
+  <div class="card-image">
+    <img src=${cardImgPng} alt="card image" class="responsive" id="cardImg">
+  </div>
+  <div class="text-box" id="testBox">
+    ${cardName}: ${cardColors}<br>
+    <hr class="solid">
+    Type: ${cardType}, Rarity: ${cardRarity}<br>
+    <hr class="solid">
+    ${cardOracleTxt}<br>
+    <hr class="solid">
+    <i>${cardFlavorText}</i><br>
+    <hr class="dotted">
+    Artist: ${cardArtist}
+  </div>
+</div>`;
 
     document.getElementById('main-content').innerHTML = (pageHtml);
-  })
-  
+} 
+
+buildPage();
+
+
+
+//  TESTING PURPOSES //////////////////////////////////////////////////////////////////////////////
+// const getSearchCard = async() => {
+//   let creatureCard = await ScryfallFetch.getSearch('creature');
+//   console.log(creatureCard);
+
+// }
+
+// getSearchCard();
+
+// const getNamedCard = async() => {
+//   let creatureCard = await ScryfallFetch.getNamed('infer reck');
+//   console.log(creatureCard);
+
+// }
+// getNamedCard();
 
 
