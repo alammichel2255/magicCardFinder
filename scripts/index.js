936343e0147047abd2069246631d704bbbe06221
Eric Sheridan <neobelkins@gmail.com>
2022-08-14 13:16:52 -0700

    Fixed all the buttons/navigation to work throught site except advanced search

--- scripts/index.js
+++ scripts/index.js
@@ -17,10 +17,11 @@ window.onload = () => {
 ///////////////////////// Navigation event triggers //////////////////////////////
 document.querySelector("#Homepage").addEventListener("click", (event) => {
   event.preventDefault();
+  document.getElementById("bg-image").style.backgroundImage = `none`;
+  document.querySelector("body").style.backgroundImage = '';
   let searchParams = new URLSearchParams(window.location.search);
   if (searchParams.has('q')){
-    searchParams.delete('q')
-    window.location.search = searchParams.toString();
+    window.history.pushState({}, document.title, "/" + '' );
   }
   document.querySelector(".searchBar").value = document.querySelector(".searchBar").defaultValue;
   buildHomePage();
@@ -28,32 +29,77 @@ document.querySelector("#Homepage").addEventListener("click", (event) => {
 
 document.querySelector("#Random-Card").addEventListener("click", (event) => {
   event.preventDefault();
+  // document.getElementById("bg-image").style.backgroundImage = `none`;
+  document.querySelector("body").style.backgroundImage = '';
   let searchParams = new URLSearchParams(window.location.search);
   if (searchParams.has('q')){
-    searchParams.delete('q')
-    window.location.search = searchParams.toString();
+    window.history.pushState({}, document.title, "/" + '' );
   }
   document.querySelector(".searchBar").value = document.querySelector(".searchBar").defaultValue;
   buildCardPage();
 });
 
-// document.querySelector("#Advanced-Search").addEventListener("click", (event) => {
-//   event.preventDefault();
-//   document.querySelector(".searchBar").value = document.querySelector(".searchBar").defaultValue;
-//   buildAdvancedSearchPage();
-// });
+document.querySelector("#Advanced-Search").addEventListener("click", (event) => {
+  event.preventDefault();
+  // document.getElementById("bg-image").style.backgroundImage = `none`;
+  document.querySelector("body").style.backgroundImage = '';
+  let searchParams = new URLSearchParams(window.location.search);
+  if (searchParams.has('q')){
+    window.history.pushState({}, document.title, "/" + '' );
+  }
+  document.querySelector(".searchBar").value = document.querySelector(".searchBar").defaultValue;
+  buildAdvancedPage();
+});
 
 document.querySelector("#About").addEventListener("click", (event) => {
   event.preventDefault();
+  // document.getElementById("bg-image").style.backgroundImage = `none`;
+  document.querySelector("body").style.backgroundImage = '';
   document.querySelector(".searchBar").value = document.querySelector(".searchBar").defaultValue;
   let searchParams = new URLSearchParams(window.location.search);
   if (searchParams.has('q')){
-    searchParams.delete('q')
-    window.location.search = searchParams.toString();
+    window.history.pushState({}, document.title, "/" + '' );
   }
   buildAboutPage();
 });
 
+/////////////// Navigation Search function //////////////////////////////
+let content = ``
+document.querySelector(".searchButton").addEventListener("click", async (event) => {
+  event.preventDefault();
+  console.log(event.target);
+  console.log(event.target.id);
+  // document.getElementById("bg-image").style.backgroundImage = `none`;
+  document.querySelector("body").style.backgroundImage = '';
+  if (document.querySelector(".searchBar").value !== "") {
+    let search = document.querySelector(".searchBar").value;
+    document.querySelector("#content-container").innerHTML = ` 
+        <div class="search-result-box">
+        </div>`;
+
+    let data = await ScryfallFetch.getSearch(search);
+    for (let i = 0; i < data.data.length; i++) {
+      if (data.data[i].image_uris) {
+        content = `
+                <div class="search-result-item">
+                    <a href="#">
+                        <img id="${data.data[i].name}" src="${data.data[i].image_uris.small}" height="300px" >
+                    </a>
+                </div>`;
+        document.querySelector(".search-result-box").innerHTML += content;
+      }
+    }
+    document.querySelector(".search-result-box").addEventListener("click", (event) => {
+      if(event.target.id !== "search-result-box" && event.target.id !== null) {
+        let searchParams = new URLSearchParams(window.location.search);
+        if (searchParams.has('q')){
+          window.history.pushState({}, document.title, "/" + '' );
+        }
+        buildCardPage(event.target.id)
+      }
+    });
+  }
+});
 
 ///////////////////////// Home Page //////////////////////////////
 const buildHomePage = async () => {
@@ -65,13 +111,22 @@ const buildHomePage = async () => {
   while (!landingImage1.image_uris){
     landingImage1 = await ScryfallFetch.getRandom();
   }
+  let landingImage2 = await ScryfallFetch.getRandom();
+  while (!landingImage2.image_uris){
+    landingImage2 = await ScryfallFetch.getRandom();
+  }
+  let landingImage3 = await ScryfallFetch.getRandom();
+  while (!landingImage3.image_uris){
+    landingImage3 = await ScryfallFetch.getRandom();
+  }
   // let landingImage2 = await ScryfallFetch.getRandom();
   // let landingImage3 = await ScryfallFetch.getRandom();
 
-  // document.getElementById("bg-image").style.backgroundImage = `url(${landingImage1.image_uris.art_crop})`;
+  document.querySelector("body").style.backgroundImage = 'none';
+  document.getElementById("bg-image").style.backgroundImage = `url(${landingImage1.image_uris.art_crop})`;
   
-  var content = ``;
-  var landing = `
+  let content = ``;
+  let landing = `
   <div class="center-search"></div>
   <h1>MagiDex</h1>
   <p> This Game Will Ruin Your Life</p>
@@ -86,6 +141,12 @@ const buildHomePage = async () => {
   <a href="">
     <img id = "${landingImage1.name}" src="${landingImage1.image_uris.small}" height=300px>
   </a>
+  <a href="">
+  <img id = "${landingImage2.name}" src="${landingImage2.image_uris.small}" height=300px>
+</a>
+<a href="">
+<img id = "${landingImage3.name}" src="${landingImage3.image_uris.small}" height=300px>
+</a>
   </div>`;
 
   document.querySelector("#content-container").innerHTML = landing;
@@ -99,41 +160,6 @@ const buildHomePage = async () => {
     
   });
   
-  //Deals with generating the search result page when using the navigation bar's search bar in the middle of the screen////////
-  document.querySelector(".searchButton").addEventListener("click", async (event) => {
-    event.preventDefault();
-    document.getElementById("bg-image").style.backgroundImage = `none`;
-    if (document.querySelector(".searchBar").value !== "") {
-      let search = document.querySelector(".searchBar").value;
-      document.querySelector("#content-container").innerHTML = ` 
-          <div class="search-result-box">
-          </div>`;
-
-      let data = await ScryfallFetch.getSearch(search);
-        for (let i = 0; i < data.data.length; i++) {
-          if (data.data[i].image_uris) {
-            content = `
-                    <div class="search-result-item">
-                        <a href="#">
-                            <img id="${data.data[i].name}" src="${data.data[i].image_uris.small}" height="300px" >
-                        </a>
-                    </div>`;
-            document.querySelector(".search-result-box").innerHTML += content;
-          }
-        }
-      document.querySelector(".search-result-box").addEventListener("click", (event) => {
-        if(event.target.id !== "search-result-box" && event.target.id !== null) {
-          let searchParams = new URLSearchParams(window.location.search);
-          if (searchParams.has('q')){
-            searchParams.delete('q')
-            window.location.search = searchParams.toString();
-          }
-          buildCardPage(event.target.id)
-        }
-      })
-    }
-  });
-
 //Deals with generating the search result page when using the landing page's search bar in the middle of the screen////////
   document.querySelector("#content-container").addEventListener("click", async (event) => {
     event.preventDefault();
@@ -159,13 +185,30 @@ const buildHomePage = async () => {
           if(event.target){
             let searchParams = new URLSearchParams(window.location.search);
             if (searchParams.has('q')){
-              searchParams.delete('q')
-              window.location.search = searchParams.toString();
+              window.history.pushState({}, document.title, "/" + '' );
             }
             buildCardPage(event.target.id)
           }
         });
       }
+    }
+    if (event.target && event.target.id === "advanced-search") {
+      event.preventDefault();
+      let searchParams = new URLSearchParams(window.location.search);
+      if (searchParams.has('q')){
+        window.history.pushState({}, document.title, "/" + '' );
+      }
+      document.querySelector(".searchBar").value = document.querySelector(".searchBar").defaultValue;
+      buildAdvancedPage();
+    }
+    if (event.target && event.target.id === "random-search") {
+      event.preventDefault();
+      let searchParams = new URLSearchParams(window.location.search);
+      if (searchParams.has('q')){
+        window.history.pushState({}, document.title, "/" + '' );
+      }
+      document.querySelector(".searchBar").value = document.querySelector(".searchBar").defaultValue;
+      buildCardPage();
     }
   });
 }
@@ -196,16 +239,17 @@ const buildCardPage = async(inputCard) => {
   // grab symbols then rip out only what the card uses into array
   let symbols = await ScryfallFetch.getSymbols();
   console.log(card.scryfall_uri);
-  // let manaCost = card.mana_cost.match(/.{1,3}/g);
-  // works but changes order to alphabetical
   let manaCost = []
-  for (let i = 0; i < card.mana_cost.length; i += 3){
-    let currentMana = '';
-    currentMana += card.mana_cost[i]
-    currentMana += card.mana_cost[i+1]
-    currentMana += card.mana_cost[i+2]
-    manaCost.push(currentMana)
+  let manaString = card.mana_cost
+
+  for (let obj of symbols){
+    while (manaString.indexOf(obj.name) >= 0){
+      let startIndex = manaString.indexOf(obj.name);
+      manaCost.push(manaString.slice(startIndex, (startIndex + obj.name.length)))
+      manaString = manaString.replace(obj.name, '')
+    }
   }
+
   // change oracle text to have images for the symbols
   let cardCMC = ''
   for (let item of manaCost){
@@ -241,7 +285,12 @@ const buildCardPage = async(inputCard) => {
   }
   let cardArtist = card.artist;
   let cardName = card.name;
-  let cardHealth = `${card.power}/${card.toughness}`
+  let cardHealth
+  if(card.power){
+    cardHealth = `${card.power}/${card.toughness}`
+  } else {
+    cardHealth = ''
+  }
   // console.log(card)
   
   document.getElementById("bg-image").style.backgroundImage = `url(${cardImgArt})`;
@@ -256,12 +305,12 @@ const buildCardPage = async(inputCard) => {
     <h2>Card Information</h2>
     ${cardName}  ${cardCMC}<br> 
     <hr class="rounded">
-    Type: ${cardType}, Rarity: ${cardRarity}<br>
+    Type: ${cardType} | Rarity: ${cardRarity}<br>
     <hr class="solid">
     ${cardOracleTxt}<br>
+    <p class="alignright"><strong>${cardHealth}</strong></p>
     <hr class="solid">
     <i>${cardFlavorText}</i><br>
-    Power: <b>${card.power}</b> / Toughness: <b>${card.toughness}</b>
     <hr class="dotted">
     Artist: ${cardArtist}<br>
     <hr class="rounded">
@@ -358,7 +407,7 @@ const buildAboutPage = () => {
   </div>
 
   <br>
-  <h2>Resources</h2>
+  <h2>Resources</h2><br>
     <a id="about-link1" href="https://magic.wizards.com/en/how-to-play" target="_blank">How To Play</a>
     <!-- <a id="about-link" href="#" target="_blank">FAQs</a> -->
     <!-- <a id="about-link" href="#" target="_blank">Twitter</a> -->
@@ -377,4 +426,169 @@ const buildAboutPage = () => {
     console.log(event);
     window.open("https://github.com/adpears94/sdi-blended-project1-scaffold", "_blank");
   });
+}
+
+///////////////////////// Advanced Page //////////////////////////////
+
+const buildAdvancedPage = () => {
+  let advancedHtml = `
+  <section class="hero">
+  <div class="container">
+      <div class="lext-col">
+          <h2>Searching for something different?</h2>
+      
+      </div>
+  </div>
+  </section>
+  <div class="about-page">
+  <div? class="advanced-filtering">
+  <div class="form-row">
+
+      <label class="form-row-label" for="name"><h2>Card Name:</h2></label>  
+      <div class="form-row-content">        
+          <input id="name-search" class="nav-item active search-bar" type="search" placeholder="Any words in the name, e.g. 'Fire'" autocomplete="off" maxlength="100" minlength="0">
+      </div>
+
+    
+      <label class="form-row-label" for="name"><h2>Text:</h2></label> 
+          <input id="text-search" class="nav-item active search-bar" type="search" placeholder="Any test, e.g. 'draw a card'" autocomplete="off" maxlength="100" minlength="0">
+
+      
+          <label class="form-row-label" for="name"><h2>Type Line</h2></label> 
+          <input id="type-line-search" class="nav-item active search-bar" type="search" placeholder="Enter a type or choose from the list" autocomplete="off" maxlength="100" minlength="0">
+
+        
+  </div>
+
+  <div class = "form-row">            
+      <label class="form-row-label" for="name"><h2>Colors:</h2></label> 
+    
+    <div class="white-box">
+      <label id="White-box" class="container">White
+        <input type="checkbox">
+        <span class="checkmark"></span>
+      </label>
+      <label id="Blue-box" class="container">Blue
+        <input type="checkbox">
+        <span class="checkmark"></span>
+      </label>
+      <label id="Black-box" class="container">Black
+        <input type="checkbox">
+        <span class="checkmark"></span>
+      </label>
+      <label id="Red-box" class="container">Red
+        <input type="checkbox">
+        <span class="checkmark"></span>
+      </label>
+      <label id="Green-box" class="container">Green
+        <input type="checkbox">
+        <span class="checkmark"></span>
+      </label>
+      <label id="Colorless-box" class="container">Colorless
+        <input type="checkbox">
+        <span class="checkmark"></span>
+      </label>
+      </div>
+    </div>    
+      <!-- <div class="dropdown">
+          <button class="dropbtn">Dropdown 
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content">
+            <a href="#">Exactly these colors</a>
+            <a href="#">Including these colors</a>
+            <a href="#">At most these colors</a>
+          </div>
+      </div> -->
+  <div class="form-row">  
+    <div class="commander">
+      <label class="form-row-label" for="commander"><h2>Commander:</h2></label> 
+        
+    <div class="white-box">   
+      <label  class="container">White
+        <input  type="checkbox">
+        <span class="checkmark"></span>
+      </label>
+      <label id="Blue-box" class="container">Blue
+        <input type="checkbox">
+        <span class="checkmark"></span>
+      </label>
+      <label id="Black-box" class="container">Black
+        <input type="checkbox">
+        <span class="checkmark"></span>
+      </label>
+      <label id="Red-box" class="container">Red
+        <input type="checkbox">
+        <span class="checkmark"></span>
+      </label>
+      <label id="Green-box" class="container">Green
+        <input type="checkbox">
+        <span class="checkmark"></span>
+      </label>
+      <label id="Colorless-box" class="container">Colorless
+        <input type="checkbox">
+        <span class="checkmark"></span>
+      </label>
+      </div>
+    </div> 
+    </div>
+  <div class="form-row">
+      <label class="form-row-label" for="mana-cost"><h2>Mana Cost:</h2></label> 
+      <input id="search-by-mana" class="nav-item active search-bar" type="search" placeholder="Any mana cost, e.g. '{W}{W}'" autocomplete="off" maxlength="100" minlength="0">
+      <!-- <label class="container">Any mana cost, e.g. "{W}{W}"
+          <input type="checkbox">
+          <span class="checkmark"></span>
+      </label> -->
+    </div>
+  <div class="form-row">
+    <div class="white-box">
+    <label class="form-row-label" for="rarity"><h2>Rarity:</h2></label> 
+    <div class="white-box">   
+      <label  class="container">Common
+        <input  type="checkbox">
+        <span class="checkmark"></span>
+      </label>
+      <label id="Blue-box" class="container">Uncommon
+        <input type="checkbox">
+        <span class="checkmark"></span>
+      </label>
+      <label id="Black-box" class="container">Rare
+        <input type="checkbox">
+        <span class="checkmark"></span>
+      </label>
+      <label id="Red-box" class="container">Mythic Rare
+        <input type="checkbox">
+        <span class="checkmark"></span>
+      </label>
+    </div>
+  </div>
+  </div> 
+
+    <div class="form-row">
+      <label class="form-row-label" for="artist"><h2>Artist:</h2></label> 
+      <input class="nav-item active search-bar" type="search" placeholder="Any artist name, e.g. 'Magali'" autocomplete="off" maxlength="100" minlength="0">
+    </div>  
+  <div class="form-row">
+      <label class="form-row-label" for="flavor-text"><h2>Flavor Text:</h2></label> 
+      <input class="nav-item active search-bar" type="search" placeholder="Any text, especially names. e.g. 'Kjerldoran'" autocomplete="off" maxlength="100" minlength="0">
+      </div>
+    <div class="form-row">  
+      <label class="form-row-label" for="lore-finder"><h2>Lore Finder:</h2></label> 
+      <input class="nav-item active search-bar" type="search" placeholder="Any text, especially names. e.g. 'Jhoira'" autocomplete="off" maxlength="100" minlength="0">
+  </div>
+</div
+  <div class="search-buttons">
+    <div class="nav-item">   
+      <button id="searchButton" class="search-buttons-long">Search using these options</button>
+    </div>
+  <div class="nav-item">
+    
+      <button  id="searchButton"  class="search-buttons-long" >Clear Filters</button>
+    </div>
+  </div>
+  </div>
+  </section>`;
+
+  document.querySelector("#content-container").innerHTML = advancedHtml;
+
 }
